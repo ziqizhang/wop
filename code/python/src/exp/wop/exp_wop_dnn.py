8fa20118ca2cb4f92cea120c9aa2a94243d68ab0
zz <ziqizhang.email@gmail.com>
2019-06-27 16:25:16 +0100

    settings

--- code/python/src/exp/wop/exp_wop_dnn.py
+++ code/python/src/exp/wop/exp_wop_dnn.py
@@ -37,23 +37,22 @@ def merge(input_columns:list, df: pd.DataFrame):
 
 
 def run_single_setting(setting_file, home_dir, remove_rare_classes,
-                       remove_no_desc_instances):
+                       remove_no_desc_instances,
+                       overwrite_params=None):
     properties = exp_util.load_properties(setting_file)
 
-    csv_training_text_data = home_dir + properties['training_text_data']
 
-    # this is the folder containing other gazetteer based features that are already pre-extracted
-    csv_training_other_feaures = home_dir + properties['training_other_features']
-    if len(csv_training_other_feaures) == 0:
-        csv_training_other_feaures = None
+    csv_training_text_data = home_dir + load_setting('training_text_data',properties, overwrite_params)
+    # this is the folder containing other numeric features that are already pre-extracted
+    csv_training_other_feaures=home_dir+load_setting('training_other_features', properties, overwrite_params)
 
     # this is the folder to save output to
-    outfolder = home_dir + properties["output_folder"]
+    outfolder = home_dir + load_setting("output_folder",properties, overwrite_params)
 
     print("\n" + str(datetime.datetime.now()))
     print("loading embedding models...")
     # this the Gensim compatible embedding file
-    dnn_embedding_file = home_dir + properties["embedding_file"]  # "H:/Python/glove.6B/glove.840B.300d.bin.gensim"
+    dnn_embedding_file = home_dir + load_setting("embedding_file",properties, overwrite_params) # "H:/Python/glove.6B/glove.840B.300d.bin.gensim"
     gensimFormat = ".gensim" in dnn_embedding_file
     if gensimFormat:
         pretrained_embedding_models = gensim.models.KeyedVectors.load(dnn_embedding_file, mmap='r')
@@ -61,7 +60,7 @@ def run_single_setting(setting_file, home_dir, remove_rare_classes,
         pretrained_embedding_models = gensim.models.KeyedVectors. \
             load_word2vec_format(dnn_embedding_file, binary=True)
 
-    n_fold = int(properties["n_fold"])
+    n_fold = int(load_setting("n_fold",properties, overwrite_params))
 
     # in order to test different DNN architectures, I implemented a parser that analyses a string following
     # specific syntax, creates different architectures. This one here takes word embedding, pass it to 3
@@ -96,7 +95,7 @@ def run_single_setting(setting_file, home_dir, remove_rare_classes,
         print("you have chosen to remove instances whose description are empty")
         df = exp_util.remove_empty_desc_instances(df, 5)
 
-    y = df[:, int(properties['class_column'])]
+    y = df[:, int(load_setting("class_column",properties, overwrite_params))]
 
     target_classes = len(set(y))
     print("\ttotal classes=" + str(target_classes))
@@ -117,7 +116,7 @@ def run_single_setting(setting_file, home_dir, remove_rare_classes,
         y = numpy.delete(y, remove_instance_indexes)
         target_classes = len(set(y))
 
-    print('[STARTED] running settings with label=' + properties['label'])
+    print('[STARTED] running settings with label=' + load_setting("label",properties, overwrite_params))
 
     for model_descriptor in model_descriptors:
         print("\tML model=" + model_descriptor)
@@ -135,7 +134,8 @@ def run_single_setting(setting_file, home_dir, remove_rare_classes,
         else:
             dnn_embedding_mask_zero = False
 
-        input_column_sources = [x for x in properties['training_text_data_columns'].split("|")]
+        input_column_sources = \
+            [x for x in load_setting("training_text_data_columns",properties, overwrite_params).split("|")]
         # now create DNN branches based on the required input text column sources
 
         dnn_branches = []
@@ -182,16 +182,40 @@ def run_single_setting(setting_file, home_dir, remove_rare_classes,
                                y_train=y,
                                final_model=final_model,
                                outfolder=outfolder,
-                               task=properties['label'],
+                               task=load_setting('label',properties,overwrite_params),
                                model_descriptor=model_descriptor)
         print("Completed running all models on this setting file")
         print(datetime.datetime.now())
 
 
+#the program can overwrite parameters defined in setting files. for example, if you want to overwrite
+#the embedding file, you can include this as an overwrite param in the command line, but specifying
+#[embedding_file= ...] where 'embedding_file' must match the parameter name. Note that this will apply
+#to ALL settings
+def parse_overwrite_params(argv):
+    params={}
+    for a in argv:
+        if "=" in a:
+            values=a.split("=")
+            params[values[0]]=values[1]
+    return params
+
+def load_setting(param_name, properties:{}, overwrite_params:{}=None):
+    if overwrite_params is not None and param_name in overwrite_params.keys:
+        return overwrite_params[param_name]
+    else:
+        return properties[param_name]
+
 if __name__ == "__main__":
     #argv-1: folder containing all settings to run, see 'input' folder
     #argv-2: working directory
     #argv3,4:set to False
+
+    # the program can take additional parameters to overwrite existing ones defined in setting files.
+    # for example, if you want to overwrite the embedding file, you can include this as an overwrite
+    # param in the command line, but specifying [embedding_file= ...] where 'embedding_file'
+    # must match the parameter name. Note that this will apply to ALL settings
+    overwrite_params = parse_overwrite_params(sys.argv)
 
     for file in os.listdir(sys.argv[1]):
         gc.collect()
@@ -201,5 +225,6 @@ if __name__ == "__main__":
 
 
         run_single_setting(setting_file,sys.argv[2],strtobool(sys.argv[3]),
-                           strtobool(sys.argv[4]))
+                           strtobool(sys.argv[4]),
+                           overwrite_params=overwrite_params)
 
